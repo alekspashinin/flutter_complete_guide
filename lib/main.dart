95c76ca603933876e1a3862a55fa4272d9e8578f
Aleks <alekspashinin@gmail.com>
2022-05-23 22:50:28 +0200

    :wrench: First Questions List + Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(StartApp());
 
-class StartApp extends StatelessWidget {
+class StartApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return StartAppState();
+  }
+}
+
+class StartAppState extends State<StartApp> {
+
+  var questionIndex = 0;
+
+  void clickAnswer(){
+    setState(() {
+      questionIndex ++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color ?',
+      'What\'s your favorite animal ?',
+      'What\'s your favorite car ?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter First App'),
         ),
-        body: Text('Body Part'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              onPressed: clickAnswer,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: clickAnswer,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: clickAnswer,
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: clickAnswer,
+              child: Text('Answer 4'),
+            )
+          ],
+        ),
       ),
     );
   }
